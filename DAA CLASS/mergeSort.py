03f7992cfc05e0a9ff4011b4492371a8bb440294
Sehwag <132811112+SehwagKumar@users.noreply.github.com>
2024-06-06 23:28:47 +0500

    modified mergeSort.py

--- DAA CLASS/mergeSort.py
+++ DAA CLASS/mergeSort.py
@@ -1,26 +1,3 @@
-# def merge(left,right,a):
-#     nL = len(left)
-#     nR = len(right)
-#     i = 0
-#     j = 0
-#     k = 0
-#     while(i < nL and j < nR):
-#         if left[i] < right[j]:
-#             a[k] = left[i]
-#             i = i+1
-#             k = k+1
-#         else:
-#             a[k] = right[j]
-#             j = j+1
-#             k = k+1
-#     while i < nL:
-#             a[k] = left[i]
-#             i = i+1
-#             k = k+1
-#     while j < nR:
-#             a[k] = right[j]
-#             j = j+1
-#             k = k+1
 import merge
 def mergeSort(lis):
     n = len(lis)
